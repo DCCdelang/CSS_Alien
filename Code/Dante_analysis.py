89fab5b7af2fb8d1493db186b25071da910a39b9
DCCdelang <33282294+DCCdelang@users.noreply.github.com>
2021-06-18 13:44:54 +0200

    change

--- Code/Dante_analysis.py
+++ Code/Dante_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("Data/replicator_data_1.csv")
+data = pd.read_csv("Data/replicator_data_2_100032_iter.csv")
 
 data.columns=["time step", "number of clusters", "number of clusters with tokens", "number of cells", "number of particles", "number of tokens", "total internal energy", "total kinetic energy"]
 
